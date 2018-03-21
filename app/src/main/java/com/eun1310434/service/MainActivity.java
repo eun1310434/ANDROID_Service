e395fca7b93e575b3ccea0d5879417c3d8e18fe2
eun1310434 <eun1310434@naver.com>
2018-03-21 20:09:47 +0900

    -ADD Thread Service

--- app/src/main/java/com/eun1310434/service/MainActivity.java
+++ app/src/main/java/com/eun1310434/service/MainActivity.java
@@ -1,56 +1,163 @@
-/**
- * 05.03.2018
- * eun1310434@naver.com
- * https://blog.naver.com/eun1310434
- * 참고) Do it android programming
- */
+/*=====================================================================
+□ Infomation
+  ○ Data : 21.03.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference
+     - Do it android app Programming
+
+□ Function
+  ○ Activity에서 Service로 데이터 보내기
+
+□ Study
+  ○ Service
+    - 화면이 없는 상태에서 백그라운드로 실행됨
+    - 서비스느 프로세스스가 종료되어도 시스템에서 자동으로 재시작함
+    - 순서
+       01) Activity - startService() : 시작시키기
+       02) Service - onCreate() : 서비스시작
+       03) Service - onDestroy() : 서비스종료
+
+  ○ onNewIntent
+    - Intent를 통해 오는 모든 것을 처리
+    - 어떤 액티비티에서 자기 자신을 호출때 중복되어 호출하는 경우를 방지
+    - onNewIntent가 만들어진 상태에서는 onCreate 호출 안됨
+    - 일정 조건을 갖추었을 때, 자동 호출
+      01) 인텐트에 Activity Flag값 설정 : intent.addFlags(intent.FLAG_ACTIVITY_SINGLE_TOP)
+      02) Androidmanifest 에서 속성을 설정
+=====================================================================*/
 
 package com.eun1310434.service;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
+
+    Intent intent;
+
+    TextView tv_a_log;
+    TextView tv_b_log;
+    TextView tv_c_log;
+
+    Button button_a;
+    Button button_b;
+    Button button_c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("onCreate", "onCreate");
 
-        editText = (EditText) findViewById(R.id.editText);
+        intent = new Intent(this, SendToService.class);
 
-        Intent passedIntent = getIntent();
-        processIntent(passedIntent);
+        tv_a_log = (TextView) findViewById(R.id.tv_a_log);
+        tv_b_log = (TextView) findViewById(R.id.tv_b_log);
+        tv_c_log = (TextView) findViewById(R.id.tv_c_log);
+
+        button_a = (Button) findViewById(R.id.button_a);
+        button_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (button_a.getText().equals("A-Start")) {
+                    SenToService(intent, "A", "start");
+                    button_a.setText("A-Quit");
+                }else{
+                    SenToService(intent, "A", "quit");
+                    button_a.setText("A-Start");
+                    button_a.setClickable(false);
+                }
+            }
+        });
+
+        button_b = (Button) findViewById(R.id.button_b);
+        button_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (button_b.getText().equals("B-Start")) {
+                    SenToService(intent, "B", "start");
+                    button_b.setText("B-Quit");
+                }else{
+                    SenToService(intent, "B", "quit");
+                    button_b.setText("B-Start");
+                    button_b.setClickable(false);
+                }
+            }
+        });
+
+        button_c = (Button) findViewById(R.id.button_c);
+        button_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (button_c.getText().equals("C-Start")) {
+                    SenToService(intent, "C", "start");
+                    button_c.setText("C-Quit");
+                }else{
+                    SenToService(intent, "C", "quit");
+                    button_c.setText("C-Start");
+                    button_c.setClickable(false);
+                }
+            }
+        });
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
+        Log.d("onNewIntent", "onNewIntent");
+        // Intent를 통해 오는 모든 것을 처리
         // onNewIntent가 만들어진 상태에서는 onCreate 호출 안됨
-        processIntent(intent);
+        // 어떤 액티비티에서 자기 자신을 호출때 중복되어 호출하는 경우를 방지하기 위해 사용되기도 함
+
+        if (intent != null) {
+            String tag = intent.getStringExtra("tag");
+            String data = intent.getStringExtra("data");
+
+            if(tag.equals("A")){
+                if(data.equals("Quit")){
+                    button_a.setClickable(true);
+                }else{
+                    button_a.setText("A-Quit");
+                }
+                tv_a_log.append(data +"\n");
+            }
+            if(tag.equals("B")){
+                if(data.equals("Quit")){
+                    button_b.setClickable(true);
+                }else{
+                    button_b.setText("B-Quit");
+                }
+                tv_b_log.append(data +"\n");
+            }
+            if(tag.equals("C")){
+                if(data.equals("Quit")){
+                    button_c.setClickable(true);
+                }else{
+                    button_c.setText("C-Quit");
+                }
+                tv_c_log.append(data +"\n");
+            }
+        }
         super.onNewIntent(intent);
     }
 
-    private void processIntent(Intent intent) {
-        if (intent != null) {
-            String type = intent.getStringExtra("type");
-            String data = intent.getStringExtra("data");
-
-            Toast.makeText(this, "type : " + type + ", data : " + data, Toast.LENGTH_SHORT).show();
-            editText.setText(data);
-        }
-    }
-
-    public void onButton1Clicked(View v) {
-        String data = editText.getText().toString();
-        Intent intent = new Intent(this, SendToService.class);
-        intent.putExtra("type", "SendToService");
-        intent.putExtra("data", data);
+    private void SenToService(Intent intent, String tag, String request) {
+        intent.putExtra("tag", tag);
+        intent.putExtra("request", request);
         startService(intent);
     }
 
+
+    @Override
+    protected void onDestroy() {
+
+        //어플을 종료시 모든 Thread를 종료
+        //SenToService(intent, "ALL", "quit");
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/eun1310434/service/SendToService.java
+++ app/src/main/java/com/eun1310434/service/SendToService.java
@@ -1,9 +1,34 @@
-/**
- * 05.03.2018
- * eun1310434@naver.com
- * https://blog.naver.com/eun1310434
- * 참고) Do it android programming
- */
+/*=====================================================================
+□ Infomation
+  ○ Data : 21.03.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference
+     - Do it android app Programming
+
+□ Function
+  ○ Service에서 Activity 데이터 받기
+  ○ 받은데이터 Thread로 처리
+  ○ 처리된 결과 Activity로 재 전송
+
+
+□ Study
+  ○ Service
+    - 화면이 없는 상태에서 백그라운드로 실행됨
+    - 서비스느 프로세스스가 종료되어도 시스템에서 자동으로 재시작함
+    - 순서
+       01) Activity - startService() : 시작시키기
+       02) Service - onCreate() : 서비스시작
+       03) Service - onDestroy() : 서비스종료
+
+  ○ onNewIntent
+    - Intent를 통해 오는 모든 것을 처리
+    - 어떤 액티비티에서 자기 자신을 호출때 중복되어 호출하는 경우를 방지
+    - onNewIntent가 만들어진 상태에서는 onCreate 호출 안됨
+    - 일정 조건을 갖추었을 때, 자동 호출
+      01) 인텐트에 Activity Flag값 설정 : intent.addFlags(intent.FLAG_ACTIVITY_SINGLE_TOP)
+      02) Androidmanifest 에서 속성을 설정
+=====================================================================*/
 
 package com.eun1310434.service;
 
@@ -14,6 +39,11 @@ import android.util.Log;
 
 public class SendToService extends Service {
     private static final String TAG = "SendToService";
+    private ServiceThread serviceThread[] = new ServiceThread[100];
+    private int serviceThreadCount = 0;
+    private ServiceThread.OnThreadListener listener;
+
+
 
     public SendToService() {
     }
@@ -23,6 +53,13 @@ public class SendToService extends Service {
         super.onCreate();
         Log.d(TAG, "onCreate()");
         //서비스는 한번만 생성되기에 onCreate는 한번 호출
+
+        listener = new ServiceThread.OnThreadListener(){
+            @Override
+            public void SetContent(String tag, String data) {
+                SendToActivity(new Intent(getApplicationContext(), MainActivity.class), tag, data );
+            }
+        };
     }
 
     @Override
@@ -31,40 +68,49 @@ public class SendToService extends Service {
         //이후 호출되는 서비스는 onStartCommand에서 처리
 
         if (intent == null) {
-            return Service.START_STICKY;//서비스가 종료되어도 다시 자동으로 실행 됨.
+            //서비스가 종료되어도 다시 자동으로 실행 됨.
+            return Service.START_STICKY;
         } else {
-            processCommand(intent);
+            String tag = intent.getStringExtra("tag");
+            String request = intent.getStringExtra("request");
+
+            if(100 < serviceThreadCount || (tag.equals("ALL") && request.equals("quit"))){
+                for(int i = 0 ; i < serviceThread.length; i++){
+                    if (serviceThread[i] != null) {
+                        serviceThread[i].quit();
+                        serviceThread[i] = null;
+                    }
+                }
+            }else if(request.equals("start")){
+                serviceThread[serviceThreadCount] = new ServiceThread(tag, listener);
+                serviceThread[serviceThreadCount].start();
+                serviceThreadCount++;
+
+            }else if(request.equals("quit")){
+                for(int i = 0 ; i < serviceThread.length; i++){
+                    if (serviceThread[i] != null && serviceThread[i].IsServiceThread(tag)) {
+                        serviceThread[i].quit();
+                        serviceThread[i] = null;
+                        break;
+                    }
+                }
+            }
         }
 
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void processCommand(Intent intent) {
-        String type = intent.getStringExtra("type");
-        String data = intent.getStringExtra("data");
-        Log.d(TAG, "type : " + type + ", data : " + data);
 
-        /*
-        for (int i = 0; i < 3; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch(Exception e) {};
 
-            Log.d(TAG, "Waiting " + i + " seconds.");
-        }
-        */
 
-        Intent showIntent = new Intent(getApplicationContext(), MainActivity.class);
-
-        showIntent.addFlags(
-                Intent.FLAG_ACTIVITY_NEW_TASK | //
-                Intent.FLAG_ACTIVITY_SINGLE_TOP |
-                Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        showIntent.putExtra("type", "SendToActivity");
-        showIntent.putExtra("data", data + " from service.");
-        startActivity(showIntent);
-
+    private void SendToActivity(Intent intent, String tag, String data) {
+        intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK | // 화면이 없는 Service에서 화면이 있는 Activity로 데이터 전송. 매우 중요
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP | // 화면이 현재 운영중이면 재사용 하라는 뜻
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP); // 현재 다른 화면이 있으면 재거하라는 뜻
+        intent.putExtra("tag", tag);
+        intent.putExtra("data", data);
+        startActivity(intent);
     }
 
     @Override
